ce7d36bd3abc72fb26f0f8defb028662367b00f8
Lucas <lucasportojpa@gmail.com>
2017-11-25 21:31:57 -0200

    Melhora na estética do código

--- src/AnalyzerTest.java
+++ src/AnalyzerTest.java
@@ -1,29 +1,34 @@
 
 import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 public class AnalyzerTest {
 
 		@Test
 		public void testOneOkExpression() {
-		char[] c = {'(',')'};
-		Stack stack = new Stack (c.length+1);
-		assertEquals(true, (Validation.loop(c, stack)));
+			char[] c = {'(',')'};
+			Stack stack = new Stack (c.length+1);
+			assertEquals(true, (Validation.loop(c, stack)));
 
-	}
+		}
 		
 		@Test
 		public void testOneExpression() {
-		char[] c = {'(',')','{'};
-		Stack stack = new Stack (c.length+1);
-		assertEquals(false, (Validation.loop(c, stack)));
+			char[] c = {'(',')','{'};
+			Stack stack = new Stack (c.length+1);
+			assertEquals(false, (Validation.loop(c, stack)));
 		}
 		
 		@Test
 		public void testOtherExpression() {
-		char[] c = {'(','a',')'};
-		Stack stack = new Stack (c.length+1);
-		assertEquals(true, (Validation.loop(c, stack)));
+			char[] c = {'(','a',')'};
+			Stack stack = new Stack (c.length+1);
+			assertEquals(true, (Validation.loop(c, stack)));
 		}
+		
+		
+		
 		
 }
