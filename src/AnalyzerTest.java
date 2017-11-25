1520c4801a1932492b52b018c5123f218032d2fb
Lucas <lucasportojpa@gmail.com>
2017-11-25 01:26:14 -0200

    implementação de mais testes

--- src/AnalyzerTest.java
+++ src/AnalyzerTest.java
@@ -1,14 +1,29 @@
-import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class AnalyzerTest {
 
 		@Test
 		public void testOneOkExpression() {
-			char[] c = {'(',')'};
-			Stack stack = new Stack (c.length+1);
-			Assert.assertEquals(Validation.loop(c, stack), true);
+		char[] c = {'(',')'};
+		Stack stack = new Stack (c.length+1);
+		assertEquals(true, (Validation.loop(c, stack)));
 
 	}
-
+		
+		@Test
+		public void testOneExpression() {
+		char[] c = {'(',')','{'};
+		Stack stack = new Stack (c.length+1);
+		assertEquals(false, (Validation.loop(c, stack)));
+		}
+		
+		@Test
+		public void testOtherExpression() {
+		char[] c = {'(','a',')'};
+		Stack stack = new Stack (c.length+1);
+		assertEquals(true, (Validation.loop(c, stack)));
+		}
+		
 }
