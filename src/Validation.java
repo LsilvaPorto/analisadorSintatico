77b761669ee974a0d858d6c943df5b8a27fafc1c
MrDobler <arthur.dobler@gmail.com>
2017-11-26 01:00:02 -0200

    Remolção das variáveis countInsert e countRemove pois não há necessidade das mesmas

--- src/Validation.java
+++ src/Validation.java
@@ -3,20 +3,9 @@ public class Validation {
 	
 	public static boolean loop(char[] array, Stack stack) {
 		
-	int i = 0, countDelete = 0, countInsert = 0;	
-		
+	int i = 0;	
 		
 		while(i < array.length) {
-			
-			
-			//Se o elemento for (, [, { adiciona um na variável countInsert
-			if (array[i] == '(' || array[i] == '[' || array[i] == '{')
-				countInsert++;
-			
-			//Se o elemento for ), ], } adiciona um na variável countDelete
-			if (array[i] == ')' || array[i] == ']' || array[i] == '}')
-				countDelete++;
-			
 			
 			if ((array[i] == '(' || array[i] == '[' || array[i] == '{')) 
 				stack.insert(array[i]);
@@ -28,9 +17,8 @@ public class Validation {
 				stack.remove();
 
 			i++;
-					
 		}
 		
-		return (Stack.isEmpty() && countDelete == countInsert);
+		return (Stack.isEmpty());
 	}		
 }
