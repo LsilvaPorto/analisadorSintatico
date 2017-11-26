c70734eccd5240c5b4337efdccd34c4d6f06ab61
MrDobler <arthur.dobler@gmail.com>
2017-11-26 01:33:26 -0200

    Remoção da verificação se é abertura e pilha vazia, passando a responsabilidade para verifica fechamento, apenas.

--- src/Validation.java
+++ src/Validation.java
@@ -10,11 +10,12 @@ public class Validation {
 			if ((array[i] == '(' || array[i] == '[' || array[i] == '{')) 
 				stack.insert(array[i]);
 			
-			if ((array[i] == ')' || array[i] == ']' || array[i] == '}') && Stack.isEmpty()) 
-				Stack.setTop(Stack.getTop()+1);
-			
 			//Verifica se o valor lido é o fechamento para a respectiva abertura 
 			if (( array[i] == ')' || array[i] == ']' || array[i] == '}')) {
+				
+				if(Stack.isEmpty())
+					return false;
+				
 				if(array[i] == ')' && Stack.charStack[Stack.getTop()] == '(')
 					stack.remove();	
 				else if(array[i] == ']' && Stack.charStack[Stack.getTop()] == '[')
