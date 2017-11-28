c71eb8fc91a7cac8035441826a6b55a6ed916cf1
Lucas <lucasportojpa@gmail.com>
2017-11-27 22:35:10 -0200

    mudanças na classe AnalyzerTest melhorias no código

--- src/AnalyzerTest.java
+++ src/AnalyzerTest.java
@@ -9,26 +9,57 @@ public class AnalyzerTest {
 		@Test
 		public void testOneOkExpression() {
 			char[] c = {'(',')'};
-			Stack stack = new Stack (c.length+1);
-			assertEquals(true, (Validation.loop(c, stack)));
+			createStackObjectTrue(c);
 
 		}
 		
 		@Test
 		public void testOneExpression() {
 			char[] c = {'(',')','{'};
-			Stack stack = new Stack (c.length+1);
-			assertEquals(false, (Validation.loop(c, stack)));
+			//Stack stack = new Stack (c.length+1);
+			//assertEquals(false, (Validation.loop(c, stack)));
+			createStackObjectFalse(c);
 		}
 		
 		@Test
 		public void testOtherExpression() {
 			char[] c = {'(','a',')'};
-			Stack stack = new Stack (c.length+1);
-			assertEquals(true, (Validation.loop(c, stack)));
+			//Stack stack = new Stack (c.length+1);
+			//assertEquals(true, (Validation.loop(c, stack)));
+			createStackObjectTrue(c);
 		}
 		
+		@BeforeEach
+		 public void createStackObjectTrue(char[] c) {
+			 Stack stack = new Stack (c.length+1);
+			 assertEquals(true, (Validation.loop(c, stack)));
+		 }
 		
+		@BeforeEach
+		 public void createStackObjectFalse(char[] c) {
+			 Stack stack = new Stack (c.length+1);
+			 assertEquals(false, (Validation.loop(c, stack)));
+		 }
+		
+		/*
+		
+		@Test
+		public void testOneOkExpression() {
+			char[] c = {'(',')'};
+			createStackObject(c);
+		}
+			
+		@Test
+		public void testOneExpression() {
+			char[] c = {'(',')','{'};
+			createStackObject(c);
+		}
+			
+		@Test
+		public void testOtherExpression() {
+			char[] c = {'(','a',')'};
+			createStackObject(c);
+		}*/
 		
 		
 }
