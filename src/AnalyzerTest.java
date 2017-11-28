fbd22c30678392b645811a951e0b45adb62958a7
Lucas <lucasportojpa@gmail.com>
2017-11-27 23:42:00 -0200

    Implementação de mais testes

--- src/AnalyzerTest.java
+++ src/AnalyzerTest.java
@@ -6,60 +6,53 @@ import org.junit.jupiter.api.BeforeEach;
 
 public class AnalyzerTest {
 
-		@Test
-		public void testOneOkExpression() {
-			char[] c = {'(',')'};
-			createStackObjectTrue(c);
-
+	
+	@BeforeEach
+	 public void createStackObjectTrue(char[] c) {
+		 Stack stack = new Stack (c.length+1);
+		 assertEquals(true, (Validation.loop(c, stack)));
+	 }
+	
+	@BeforeEach
+	 public void createStackObjectFalse(char[] c) {
+		 Stack stack = new Stack (c.length+1);
+		 assertEquals(false, (Validation.loop(c, stack)));
+	 }
+	
+		
+	@Test
+	public void testOneFalse() {
+		char[] c = {'(','}',')'};
+		createStackObjectFalse(c);
 		}
 		
-		@Test
-		public void testOneExpression() {
-			char[] c = {'(',')','{'};
-			//Stack stack = new Stack (c.length+1);
-			//assertEquals(false, (Validation.loop(c, stack)));
-			createStackObjectFalse(c);
+	@Test
+	public void testFalseExpression() {
+		char[] c = {'(',')',')'};
+		createStackObjectFalse(c);
 		}
 		
-		@Test
-		public void testOtherExpression() {
-			char[] c = {'(','a',')'};
-			//Stack stack = new Stack (c.length+1);
-			//assertEquals(true, (Validation.loop(c, stack)));
-			createStackObjectTrue(c);
-		}
 		
-		@BeforeEach
-		 public void createStackObjectTrue(char[] c) {
-			 Stack stack = new Stack (c.length+1);
-			 assertEquals(true, (Validation.loop(c, stack)));
-		 }
 		
-		@BeforeEach
-		 public void createStackObjectFalse(char[] c) {
-			 Stack stack = new Stack (c.length+1);
-			 assertEquals(false, (Validation.loop(c, stack)));
-		 }
-		
-		/*
-		
-		@Test
-		public void testOneOkExpression() {
-			char[] c = {'(',')'};
-			createStackObject(c);
-		}
+	@Test
+	public void testOneMoreFalse() {
+		char[] c = {'(','}'};
+		createStackObjectFalse(c);
+	}
 			
-		@Test
-		public void testOneExpression() {
-			char[] c = {'(',')','{'};
-			createStackObject(c);
-		}
+	@Test
+	public void testOneExpression() {
+		char[] c = {'(',')',')'};
+		createStackObjectFalse(c);
+	}
 			
-		@Test
-		public void testOtherExpression() {
-			char[] c = {'(','a',')'};
-			createStackObject(c);
-		}*/
-		
+	@Test
+	public void testOtherExpression() {
+		char[] c = {'(',']'};
+		createStackObjectFalse(c);
+	}
+	
+	
+	
 		
 }
